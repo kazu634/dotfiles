4937940970c6420a29517e7cbf18edd2c9d8b194
Kazuhiro MUSASHI <simoom634@yahoo.co.jp>
2011-11-27 21:09:26 +0900

    Add the procedures to judge the OS types. In the case of Linux, this install script does not work properly. This problem consists of 3 causes:   (1) The position of the maxdepth parameter of the find command   (2) The difference of the find options   (3) The behaviour of the dirname and find command This commit fixes #10.

--- install.sh
+++ install.sh
@@ -3,5 +3,20 @@
 # finding out the shell script directory
 CURDIR=`dirname $0`
 
-# copying the .files as a soft link
-find $CURDIR -type f -name ".*" -maxdepth 1 -print0 | xargs -0 -J % ln -s % $HOME 2>/dev/null
+# finding out the OS type
+OS=`uname`
+
+# judging the OS type
+if [ $OS = "Linux" ]; then # if linux
+  # Linux do not interpret the current directory "." as an absolute one,
+  # so we have to change manually.
+  if [ $CURDIR = "." ]; then
+    CURDIR=`pwd`
+  fi
+
+  # copying the .files as a soft link
+  find $CURDIR -maxdepth 1 -type f -name ".*" -print0 | xargs -0 -I % ln -s % $HOME 2>/dev/null
+else # otherwise (meaning Darwin)
+  # copying the .files as a soft link
+  find $CURDIR -type f -name ".*" -maxdepth 1 -print0 | xargs -0 -J % ln -s % $HOME 2>/dev/null
+fi
