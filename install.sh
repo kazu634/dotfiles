06b400e9728249e57afaa64bf4896f33458961f9
Kazuhiro MUSASHI <simoom634@yahoo.co.jp>
2014-10-25 10:54:29 +0900

    Copying the `peco` configuration file as a soft link.

--- install.sh
+++ install.sh
@@ -14,7 +14,14 @@ fi
 if [ $OS = "Linux" ]; then # if linux
   # copying the .files as a soft link
   find $CURDIR -maxdepth 1 -type f -name ".*" -print0 | xargs -0 -I % ln -f -s % $HOME 2>/dev/null
+
+  # copying the `peco` configuration file as a soft link
+  ln -f -s ${CURDIR}/.peco ${HOME}
+
 else # otherwise (meaning Darwin)
   # copying the .files as a soft link
   find $CURDIR -type f -name ".*" -maxdepth 1 -print0 | xargs -0 -J % ln -f -s % $HOME 2>/dev/null
+
+  # copying the `peco` configuration file as a soft link
+  ln -f -s ${CURDIR}/.peco ${HOME}
 fi
